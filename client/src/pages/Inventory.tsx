f0ab348b8f9c4b0e01248ada6671c4aea6dc3b26
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-09-14 18:37:04 -0600

    Create InventoryPage.tsx and have it get data from the server.

--- client/src/pages/Inventory.tsx
+++ client/src/pages/Inventory.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "../components/Header";
+import InventoryPage from "../components/InventoryPage";
+import Footer from "../components/Footer";
 
 const Inventory = () => {
   return (
     <main className="font-sans antialiased text-gray-600 min-h-full flex flex-col">
-      <div>Hello From Inventory</div>
+      <Header />
+      <InventoryPage />
+      <Footer />
     </main>
   );
 };
