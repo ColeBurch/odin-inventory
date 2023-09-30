e7cefdb6e11c1db33f7226dd261021f251c91a64
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-09-30 13:34:53 -0600

    Remove unused SVG's

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -218,10 +218,10 @@ const Header = () => {
                   )}
                 </Disclosure>
                 <a
-                  href="#/store"
+                  href="/inventory"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  Inventory
                 </a>
                 <a
                   href="/"
