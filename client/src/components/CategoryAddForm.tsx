7a0ea836755fb98319a73553853ae7dbe6d48099
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-10-09 20:49:55 -0600

    Style CategoryAddForm.tsx and add validation.

--- client/src/components/CategoryAddForm.tsx
+++ client/src/components/CategoryAddForm.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Form, Field } from "@progress/kendo-react-form";
-import { Input } from "@progress/kendo-react-inputs";
+
+const textBoxAppearance: string =
+  "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4 mb-4 text-center";
 
 const CategoryAddForm = () => {
   const handleSubmit = (data: any, event: any) => {
@@ -8,28 +10,69 @@ const CategoryAddForm = () => {
     event.preventDefault();
   };
 
+  const Input = (fieldProps: any) => {
+    const {
+      fieldType,
+      label,
+      value,
+      visited,
+      valid,
+      onChange,
+      onBlur,
+      onFocus,
+      validationMessage,
+    } = fieldProps;
+    const invalid = !valid && visited;
+    return (
+      <div
+        onBlur={onBlur}
+        onFocus={onFocus}
+        className="flex flex-col items-center justify-center mb-2 mx-auto w-full text-red-600"
+      >
+        <label className="text-center block text-gray-700 text-xl font-bold mb-2">
+          {label}
+          <input
+            type={fieldType}
+            className={
+              invalid
+                ? "invalid border-red-600 border" + textBoxAppearance
+                : "" + textBoxAppearance
+            }
+            value={value}
+            onChange={onChange}
+          />
+        </label>
+        {invalid && <div className="required">{validationMessage}</div>}
+      </div>
+    );
+  };
+
+  const requiredValidator = (value: any) => {
+    return value ? "" : "This field is required";
+  };
+
   return (
-    <div className="h-screen bg-white mt-16">
+    <div className="w-full bg-white mt-16 min-h-full">
       <Form
         onSubmit={handleSubmit}
         initialValues={{}}
         render={(formRenderProps) => (
           <form
             onSubmit={formRenderProps.onSubmit}
-            className="flex flex-col items-center justify-center bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 mx-auto"
+            className="flex flex-col items-center justify-center bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto w-72 lg:w-1/3"
           >
             <h1 className="text-3xl font-bold mb-4">Add Category</h1>
-            <h2 className="text-xl font-bold">Category Name:</h2>
             <Field
               name="name"
               component={Input}
-              className="mt-4 mb-4 text-center"
+              label="Category Name:"
+              validator={[requiredValidator]}
             />
-            <h2 className="text-xl font-bold">Description:</h2>
             <Field
               name="description"
               component={Input}
-              className="mt-4 mb-4 text-center"
+              label="Description:"
+              validator={[requiredValidator]}
             />
             <button
               type={"submit"}
