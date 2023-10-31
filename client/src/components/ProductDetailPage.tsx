a90d73fcbca5a78ad5969c29d4b38a6b6e82278d
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-10-31 05:37:19 -0600

    Add table to product detail page.

--- client/src/components/ProductDetailPage.tsx
+++ client/src/components/ProductDetailPage.tsx
@@ -50,8 +50,12 @@ const ProductDetailPage = () => {
         .get("http://localhost:3000/api/productinstances/" + id)
         .then((res) => {
           if (res.data) {
-            setProductInstances(res.data);
-            console.log(res.data);
+            const sortedInstances = res.data.sort((a: any, b: any) => {
+              return (
+                a.color.localeCompare(b.color) || a.size.localeCompare(b.size)
+              );
+            });
+            setProductInstances(sortedInstances);
           }
         })
         .catch((err) => {
@@ -66,14 +70,44 @@ const ProductDetailPage = () => {
   return (
     <div>
       <div>{productDetails.name}</div>
-      <div>
-        {productInstances.map((productInstance) => (
-          <div key={productInstance._id} className="flex gap-1">
-            <div>{productInstance.color}</div>
-            <div>{productInstance.size}</div>
-            <div>{productInstance.quantity}</div>
-          </div>
-        ))}
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+        <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                Size
+              </th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                Quantity
+              </th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {productInstances.map((productInstance) => (
+              <tr className="hover:bg-gray-50">
+                <th className="px-6 py-4 font-normal text-gray-900">
+                  {productInstance.color}
+                </th>
+                <td className="px-6 py-4">{productInstance.size}</td>
+                <td className="px-6 py-4">{productInstance.quantity}</td>
+                <td className="px-6 py-4">
+                  <button className="bg-gray-100 text-gray-700 px-2 py-1 rounded-md hover:bg-gray-200">
+                    Edit
+                  </button>
+                  <button className="bg-gray-100 text-gray-700 px-2 py-1 rounded-md hover:bg-gray-200">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
